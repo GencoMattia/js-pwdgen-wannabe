c0c32aca5b0896f80850259de50fe5a14fdf97e7
GencoMattia <gencomattia@hotmail.it>
2024-05-07 15:06:26 +0200

    Added js functions

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// chiedi all'utente il nome
+// chiedi all'utente il cognome
+// chiedi aòò'utente il colore preferito
+// stampa su documento  "nome" + "cognome" + "colore preferito" + "24"
+
+const userName = prompt("Qual è il tuo nome?");
+
+const userSurname = prompt("Qual è il tuo cognome?");
+
+const favoriteColor = prompt("Qual è il tuo colore preferito?");
+
+console.log(userName + userSurname + favoriteColor + "24");
+
+document.writeln(userName + userSurname + favoriteColor + "24");
